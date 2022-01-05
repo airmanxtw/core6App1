9d32cf6e6490ea314da0cde6f3afa9f5775c94da
airmanxtw <airmanx@stust.edu.tw>
2022-01-05 10:05:14 +0800

    move addcors to build up

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("PublicPolicy",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,10 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-builder.Services.AddCors(options => {
-    options.AddPolicy("PublicPolicy",
-                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-});
+
 
 
 app.UseHttpsRedirection();
